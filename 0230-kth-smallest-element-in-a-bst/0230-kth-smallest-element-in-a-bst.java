eb319c4bc44b58f0942806f7627b7f8ca8105273
k-javaman <121099442+k-javaman@users.noreply.github.com>
2023-02-11 00:02:01 +0900

    Time: 1 ms (60.64%), Space: 42.7 MB (41.95%) - LeetHub

--- 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.java
+++ 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.java
@@ -19,10 +19,10 @@ class Solution {
         inorder(root, list);
         return list.get(k-1);
     }
-
+    
     private void inorder(TreeNode root, List<Integer> list) {
         if(root == null) return;
-
+        
         inorder(root.left, list);
         list.add(root.val);
         inorder(root.right, list);
