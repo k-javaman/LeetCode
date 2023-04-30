45da2864b2c746dfebfecb474d93814f8b16fa6f
k-javaman <121099442+k-javaman@users.noreply.github.com>
2023-04-30 16:09:01 +0900

    Time: 96 ms (5.01%), Space: 16.8 MB (13.38%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -1,17 +1,20 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        # extra memory for new string
+        # Create a new string that contains only the alphanumeric characters of the input string in lowercase
         newStr = ""
 
         for c in s:
+            # Check if the character is alphanumeric using the alphaNum() method
             if self.alphaNum(c):
+                # Add the lowercase version of the character to newStr
                 newStr += c.lower()
-                # [::-1] --> reverse order
+
+        # Check if newStr is equal to its reverse (i.e., if it is a palindrome)
         return newStr == newStr[::-1]
-    
+
     def alphaNum(self, c):
+        # Check if the ASCII value of the input character falls within certain ranges
         return (ord('A') <= ord(c) <= ord('Z') or
-               ord('a') <= ord(c) <= ord('z') or
-               ord('0') <= ord(c) <= ord('9'))
-            
+                ord('a') <= ord(c) <= ord('z') or
+                ord('0') <= ord(c) <= ord('9'))
         
