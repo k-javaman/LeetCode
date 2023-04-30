6363385235318f525e26acb90482d6860a7367de
k-javaman <121099442+k-javaman@users.noreply.github.com>
2023-04-30 15:59:36 +0900

    Time: 89 ms (5.01%), Space: 16.9 MB (13.38%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -4,9 +4,14 @@ class Solution:
         newStr = ""
 
         for c in s:
-            if c.isalnum():
+            if self.alphaNum(c):
                 newStr += c.lower()
                 # [::-1] --> reverse order
         return newStr == newStr[::-1]
+    
+    def alphaNum(self, c):
+        return (ord('A') <= ord(c) <= ord('Z') or
+               ord('a') <= ord(c) <= ord('z') or
+               ord('0') <= ord(c) <= ord('9'))
             
         
