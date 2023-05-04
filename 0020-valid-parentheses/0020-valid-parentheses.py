55478ee7e56760de40fd820ffc9f74ef068af517
k-javaman <121099442+k-javaman@users.noreply.github.com>
2023-05-05 01:25:04 +0900

    Time: 47 ms (9.55%), Space: 16.4 MB (7.05%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.py
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -3,27 +3,22 @@ class Solution:
         # Create an empty stack to store opening brackets
         stack = []
 
-        # Iterate over each character in the string
-        for char in s:
-            # If the character is an opening bracket, push it onto the stack
-            if char == '(' or char == '{' or char == '[':
-                stack.append(char)
-            else:
-                # If the stack is empty and we encounter a closing bracket, the brackets are not balanced
-                if not stack:
-                    return False
+        # Create a dictionary to map closing brackets to their corresponding opening brackets
+        closeToOpen = { ")" : "(", "]" : "[", "}": "{"}
 
-                # If the character is a closing bracket, pop the top element from the stack and
-                # check if it matches the corresponding opening bracket
-                if char == ')' and stack[-1] == '(':
-                    stack.pop()
-                elif char == '}' and stack[-1] == '{':
-                    stack.pop()
-                elif char == ']' and stack[-1] == '[':
+        # Iterate over each character in the string
+        for c in s:
+            # If the character is a closing bracket, check if it matches the top element of the stack
+            # if stack -> if stack is not empty,
+            if c in closeToOpen:
+                if stack and stack[-1] == closeToOpen[c]:
                     stack.pop()
                 else:
                     # If the brackets do not match, they are not balanced
                     return False
+            else:
+                # If the character is an opening bracket, push it onto the stack
+                stack.append(c)
 
         # If there are any remaining opening brackets on the stack, the brackets are not balanced
-        return not stack
+        return True if not stack else False
