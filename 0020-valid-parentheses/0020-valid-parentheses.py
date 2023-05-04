ffc25a2b05663b1e2225056b2dd685230634d220
k-javaman <121099442+k-javaman@users.noreply.github.com>
2023-05-05 01:20:47 +0900

    Time: 52 ms (5.13%), Space: 16.3 MB (7.05%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.py
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -1,16 +1,29 @@
 class Solution:
     def isValid(self, s: str) -> bool:
+        # Create an empty stack to store opening brackets
         stack = []
-        closeToOpen = { ")" : "(", "]" : "[", "}": "{"}
 
-        for c in s:
-            if c in closeToOpen:
-                if stack and stack[-1] == closeToOpen[c]:
+        # Iterate over each character in the string
+        for char in s:
+            # If the character is an opening bracket, push it onto the stack
+            if char == '(' or char == '{' or char == '[':
+                stack.append(char)
+            else:
+                # If the stack is empty and we encounter a closing bracket, the brackets are not balanced
+                if not stack:
+                    return False
+
+                # If the character is a closing bracket, pop the top element from the stack and
+                # check if it matches the corresponding opening bracket
+                if char == ')' and stack[-1] == '(':
+                    stack.pop()
+                elif char == '}' and stack[-1] == '{':
+                    stack.pop()
+                elif char == ']' and stack[-1] == '[':
                     stack.pop()
                 else:
+                    # If the brackets do not match, they are not balanced
                     return False
-            else:
-                stack.append(c)
-                
-        return True if not stack else False
-        
+
+        # If there are any remaining opening brackets on the stack, the brackets are not balanced
+        return not stack
