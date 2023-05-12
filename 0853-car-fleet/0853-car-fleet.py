e771c5cf68271290f8dcba4378ae008a9e032769
k-javaman <121099442+k-javaman@users.noreply.github.com>
2023-05-13 04:13:55 +0900

    Time: 838 ms (99.06%), Space: 37.9 MB (25.24%) - LeetHub

--- 0853-car-fleet/0853-car-fleet.py
+++ 0853-car-fleet/0853-car-fleet.py
@@ -1,9 +1,9 @@
 class Solution:
     def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
-        ans = prev = 0
-        for pp, ss in sorted(zip(position, speed), reverse=True):
-            tt = (target - pp)/ss # time to arrive at target 
-            if  prev < tt:
-                ans += 1
-                prev = tt
-        return ans 
+        ans = prev = 0 # Initialize ans and prev to 0
+        for pp, ss in sorted(zip(position, speed), reverse=True): # Sort the position and speed arrays in descending order of position
+            tt = (target - pp)/ss # Calculate the time it takes for the car to reach the target
+            if  prev < tt: # If the time it takes to reach the target is greater than the previous car's time
+                ans += 1 # Increment the number of car fleets
+                prev = tt # Update the previous car's time
+        return ans # Return the number of car fleets
