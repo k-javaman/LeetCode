441bd4d61955d2a2b110a6e5732a2a210546fe02
k-javaman <121099442+k-javaman@users.noreply.github.com>
2023-05-17 02:37:41 +0900

    Time: 59 ms (20.20%), Space: 16.8 MB (16.12%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.py
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.py
@@ -5,6 +5,7 @@ class Solution:
         top, bot = 0, ROWS - 1
         while top <= bot:
             row = (top + bot) // 2
+            # [-1] : rightmost item
             if target > matrix[row][-1]:
                 top = row + 1
             elif target < matrix[row][0]:
