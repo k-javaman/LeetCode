36ea96872dd448424ac9ff1db8af3702ca086866
k-javaman <121099442+k-javaman@users.noreply.github.com>
2023-06-08 01:24:12 +0900

    Time: 66 ms (5.87%), Space: 16.7 MB (51.25%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
@@ -1,33 +1,23 @@
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
-        # Initialize left and right pointers
         l, r = 0, len(nums) - 1
 
-        # While the search space is not empty
         while l <= r:
-            # Calculate the mid point
-            mid = (l + r) // 2
+            m = (l + r) // 2
+            if nums[m] == target:
+                return m
 
-            # If the target is found, return its index
-            if target == nums[mid]:
-                return mid
-
-            # If the left half is sorted
-            if nums[l] <= nums[mid]:
-                # If target is greater than mid or smaller than left pointer, it should be in the right half
-                if target > nums[mid] or target < nums[l]:
-                    l = mid + 1
-                # Else it should be in the left half
+            # if the left part is sorted
+            if nums[l] <= nums[m]:
+                if nums[l] > target or nums[m] < target:
+                    l = m + 1
                 else:
-                    r = mid - 1
-            # If the right half is sorted
+                    r = m - 1
+
+            # if the right part is sorted
             else:
-                # If target is less than mid or greater than right pointer, it should be in the left half
-                if target < nums[mid] or target > nums[r]:
-                    r = mid - 1
-                # Else it should be in the right half
+                if nums[m] > target or nums[r] < target:
+                    r = m - 1
                 else:
-                    l = mid + 1
-
-        # If we reach here, that means the element was not found
+                    l = m + 1
         return -1
