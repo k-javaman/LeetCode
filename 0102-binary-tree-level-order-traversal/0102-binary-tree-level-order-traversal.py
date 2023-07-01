9658db3297a20513b1f8d5c01fa75e2809a5f911
k-javaman <121099442+k-javaman@users.noreply.github.com>
2023-07-02 07:06:22 +0900

    Time: 57 ms (46.37%), Space: 17 MB (82.34%) - LeetHub

--- 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.py
+++ 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.py
@@ -11,18 +11,17 @@ class Solution:
         q = collections.deque()
         if root:
             q.append(root)
-
+            
         while q:
-            val = []
-
+            level = []
             for i in range(len(q)):
                 node = q.popleft()
-                val.append(node.val)
+                level.append(node.val)
                 if node.left:
                     q.append(node.left)
                 if node.right:
                     q.append(node.right)
-            res.append(val)
+            res.append(level)
         return res
             
             
