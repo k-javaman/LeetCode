de1f5538a5c94a2a4ecafd354fdb369dbc3174ac
k-javaman <121099442+k-javaman@users.noreply.github.com>
2023-07-05 02:21:43 +0900

    Time: 64 ms (37.61%), Space: 19 MB (28.13%) - LeetHub

---
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.py
@@ -0,0 +1,19 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+# The Solution class containing the method goodNodes
+class Solution:
+    # The function takes in a binary tree root node as an argument
+    def isValidBST(self, root: Optional[TreeNode]) -> bool:
+        def valid(node, left, right):
+            if not node:
+                return True
+            if not (node.val > left and node.val < right):
+                return False
+            return valid(node.left, left, node.val) and valid(node.right, node.val, right)
+        
+        return valid(root, float("-inf"), float("inf"))
+        
