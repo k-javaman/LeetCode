d91a4d513e9ea4f64d4126bc42a6339360f7f287
k-javaman <121099442+k-javaman@users.noreply.github.com>
2023-07-07 02:31:27 +0900

    Time: 46 ms (69.68%), Space: 16.4 MB (32.62%) - LeetHub

---
+++ 1046-last-stone-weight/1046-last-stone-weight.py
@@ -0,0 +1,17 @@
+class Solution:
+    # Method definition, takes a list of integers "stones" as input
+    def lastStoneWeight(self, stones: List[int]) -> int:
+
+        stones = [-s for s in stones]
+        heapq.heapify(stones)
+
+        while len(stones) > 1:
+            first = heapq.heappop(stones)
+            second = heapq.heappop(stones)
+
+            if second != first:
+                heapq.heappush(stones, first - second)
+
+        stones.append(0)
+        return abs(stones[0])
+        
