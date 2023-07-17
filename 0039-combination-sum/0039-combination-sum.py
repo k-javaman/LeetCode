61a0ad91ef27bd969d27cf3a0d76483d99f20736
k-javaman <121099442+k-javaman@users.noreply.github.com>
2023-07-18 01:33:03 +0900

    Time: 71 ms (79.17%), Space: 16.6 MB (25.31%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.py
+++ 0039-combination-sum/0039-combination-sum.py
@@ -6,7 +6,7 @@ class Solution:
         res = []
 
         # Define a helper function dfs that performs Depth-First Search. It takes an index 'i', a list 'cur' and a sum 'total'
-        def dfs(i, cur, total):
+        def dfs(cur, i, total):
             # If total equals the target, it means we found a valid combination, so append it to 'res'
             if total == target:
                 res.append(cur.copy())
@@ -18,13 +18,13 @@ class Solution:
             # We add the current candidate to the list 'cur' and the sum 'total'
             cur.append(candidates[i])
             # Call the dfs function recursively, keeping the same index 'i'. This allows us to reuse the same number
-            dfs(i, cur, total + candidates[i])
+            dfs(cur, i, total + candidates[i])
             # We remove the last added candidate. This is known as backtracking, and is necessary to search for all possible combinations
             cur.pop()
             # Call the dfs function recursively, but incrementing the index 'i' to move on to the next candidate
-            dfs(i + 1, cur, total)
+            dfs(cur, i+1, total)
 
         # We kick off the Depth-First Search with the initial call of dfs function
-        dfs(0, [], 0)
+        dfs([], 0, 0)
         # We return the list of all combinations that add up to the target
         return res
