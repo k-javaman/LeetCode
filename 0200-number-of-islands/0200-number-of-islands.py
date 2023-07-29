d385b2c0b9d7e1a2c00ca709aa7f87cbe189532f
k-javaman <121099442+k-javaman@users.noreply.github.com>
2023-07-30 02:43:27 +0900

    Time: 269 ms (99.89%), Space: 19 MB (66.86%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -1,36 +1,17 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-          
-        if grid is None:
-            return 0
-
-        m = len(grid)
-
-        if m == 0:
-            return 0
-        
-        n = len(grid[0])
-        if(n==0):
-            return 0
-        
+        if not grid: return 0
+        m, n = len(grid), len(grid[0])
         ans = 0
-
+        def dfs(i, j):
+            grid[i][j] = '2'
+            for di, dj in (0, 1), (0, -1), (1, 0), (-1, 0):
+                ii, jj = i+di, j+dj
+                if 0 <= ii < m and 0 <= jj < n and grid[ii][jj] == '1':
+                    dfs(ii, jj)
         for i in range(m):
             for j in range(n):
-
-                if(grid[i][j]=="1"):
-                    ans +=1
-                    self.dfs(grid, i,j,m,n)
-        
+                if grid[i][j] == '1':
+                    dfs(i, j)
+                    ans += 1
         return ans
-    
-    def dfs(self, g, i, j, m, n):
-        if i<0 or j<0 or i>=m or j>=n or g[i][j] == "0":
-            return
-        
-        g[i][j] = "0"
-
-        self.dfs(g,i-1,j,m,n)
-        self.dfs(g,i+1,j,m,n)
-        self.dfs(g,i,j-1,m,n)
-        self.dfs(g,i,j+1,m,n)
